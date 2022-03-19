e554367bcfdcc4d13146ead30832b7d70c6488a8
dnjmn <dhananjay21iitd@gmail.com>
2022-03-20 02:46:21 +0530

    zsh setup with omz

---
+++ config/zsh/aliases.zsh
@@ -0,0 +1,46 @@
+#source $ZDOTDIR/kbaliases
+#source $ZDOTDIR/kbenv
+## SYSTEM WIDE
+# listing
+alias l='lsd'
+alias ll='lsd -l'
+alias la='lsd -la'
+alias lt='lsd --tree'
+
+alias cdd='cd $DOTFILES'
+alias cdc='cd $CREDFILES'
+alias cdi='cd /Users/metalhead/Projects/developer/languages/golang/playground/interview'
+
+
+## GIT
+# git add
+alias ga='git add'
+alias gaa='git add --all'
+# git status / *f -> full 
+alias gs='git status -sb'
+alias gsf='git status'
+# commit
+alias gc='git commit -m'
+# log
+alias gl='git log --oneline'
+alias glf='git log'
+alias gll='git log -1 HEAD --stat'
+# diff
+alias gd='git diff'
+# difftool
+alias gdt='git difftool'
+# config
+alias gcon='git config --list'
+
+## TERMINAL
+alias c='clear'
+
+## SSH
+alias sshp14sn='ssh p14s -t tmux new -s p14s'
+alias sshp14s='ssh p14s -t tmux a -t p14s'
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    alias v='~/./nvim.appimage'
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    alias v='nvim'
+fi
---
+++ config/zsh/init.zsh
@@ -0,0 +1,16 @@
+export DOTFILES="$HOME/Github/dnjmn/private/dotfiles"
+export CREDFILES="$HOME/Github/dnjmn/private/credfiles"
+#
+#-- XDG Paths --------------------------------------------------------------------------------
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
+export XDG_DATA_DIRS="/usr/local/share:/usr/share"
+export XDG_CONFIG_DIRS="/etc/xdg"
+# export XDG_RUNTIME_DIR ??
+
+
+#-- zsh --------------------------------------------------------------------------------------
+export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
+source $ZDOTDIR/.zshenv
