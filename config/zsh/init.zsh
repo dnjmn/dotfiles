304f5abfb7dc9a83aca445be80411c1f2e094a37
dnjmn <dhananjay21iitd@gmail.com>
2022-04-03 14:25:21 +0530

    work zsh files source added

--- config/zsh/init.zsh
+++ config/zsh/init.zsh
@@ -1,6 +1,6 @@
 export DOTFILES="$HOME/Github/dnjmn/private/dotfiles"
 export CREDFILES="$HOME/Github/dnjmn/private/credfiles"
-#
+
 #-- XDG Paths --------------------------------------------------------------------------------
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
@@ -9,7 +9,6 @@ export XDG_STATE_HOME="$HOME/.local/state"
 export XDG_DATA_DIRS="/usr/local/share:/usr/share"
 export XDG_CONFIG_DIRS="/etc/xdg"
 # export XDG_RUNTIME_DIR ??
-
 
 #-- zsh --------------------------------------------------------------------------------------
 export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
